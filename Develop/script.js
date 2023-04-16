142fca22b98fc852d5974268fe4ff015437de6f7
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-04-15 23:53:35 -0400

    Got turned around trying to make eventListener for local storage buttons.

--- Develop/script.js
+++ Develop/script.js
@@ -2,15 +2,24 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
+// Added timeNow variable, which displays the current date/time within the header.
+var timeNow = dayjs()
+  $('#currentDay').text(timeNow.format('dddd, MMMM D YYYY, h:mm a'));
+  console.log(timeNow);
 
 
-$(function () {
+
+$() (function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  
+
+  
   
   
   // var saveButtons = document.querySelectorAll('.saveBtn');
@@ -18,7 +27,7 @@ $(function () {
   
   //   for (i=0; saveButtons.length; i++) {
   //     saveButtons[i].addEventListener('click', function(){
-  //       var calEvent
+  //       var blockContent = 
 
 
   //       localStorage.setItem()
@@ -44,14 +53,12 @@ $(function () {
   // current hour in 24-hour time?
   //
   
-  // Added timeNow variable, which displays the current date/time within the header.
-var timeNow = dayjs()
-  $('#currentDay').text(timeNow.format('dddd, MMMM D YYYY, h:mm a'));
-  console.log(timeNow);
+  
+//Removing hourNow variable as I think it was a misstep.
 
-var hourNow = dayjs().format('H')
-  console.log(hourNow);
-  console.log(timeNow);
+// var hourNow = dayjs().format('H')
+//   console.log(hourNow);
+//   console.log(timeNow);
   
 // Added variables for the hours of the workday, assigning them to the IDs of their respetive hour blocks. Next step is hopefully to create conditional statements to determine color.
 var nineAm = $("#hour-9")
@@ -226,4 +233,20 @@ console.log(nineAmTime);
     sixPm.removeClass('future');
     sixPm.addClass('past');
   }
+
+
+
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+
+
 })
