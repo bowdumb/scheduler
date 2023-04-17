ddb979eae83d5f5009716ca1b77caa4d1f27bde7
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-04-17 00:36:40 -0400

    cleaned up abandoned code, fleshed out README.md, and added appropriate comments within the JavaScript file.

--- Develop/script.js
+++ Develop/script.js
@@ -1,54 +1,9 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
-// //Removing hourNow variable as I think it was a misstep.
-// var hourNow = dayjs().format('H')
-//   console.log(hourNow);
-//   // console.log(timeNow);
-//   // console.log(nineAmTime);
-
-
- //   console.log(saveButtons);
-  
-  //   for (i=0; saveButtons.length; i++) {
-  //     saveButtons[i].addEventListener('click', function(){
-  //       var blockContent = 
-
-
-  //       localStorage.setItem()
-  //     })
-  //   }
-
- 
-
-  // saveButtons.on('click', function() {
-    
-  //   var btnNodes = saveButtons[i]
-    
- 
-
-
-  // } )
-
-
-
-
- // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-
-
 
 $(function () {
   var timeNow = dayjs()
-$('#currentDay').text(timeNow.format('dddd, MMMM D YYYY, h:mm a'));
-  var $saveButtons = $('.saveBtn');
+$('#currentDay').text(timeNow.format('dddd, MMMM D YYYY, h:mm a')); //Returns the current local time from the timeNow variable above, and formats it as written.
+  var $saveButtons = $('.saveBtn'); //Targets all of the save buttons, and groups them as an object. Below they are used to target the appropriate text boxes for time blocks in order to enable saving to local storage.
+  
 
 
 
@@ -59,27 +14,23 @@ $('#currentDay').text(timeNow.format('dddd, MMMM D YYYY, h:mm a'));
     var timeBlockEl = $thisButton.parent()
     var timeBlockId = timeBlockEl.attr('id')
     
-    localStorage.setItem(timeBlockId,inputValue)
-
-    
-   
-    
+    localStorage.setItem(timeBlockId,inputValue) //Determines the key:value combination and saves it to local storage for recall on lines 50-58.
    
   });
 
-  
+  //Functions as a for loop, allowing us to loop through the id values assigned to each time block div.
   $('.time-block').each(function(){
     var $thisTimeBlock = $(this);
-    var timeBlockId = $thisTimeBlock.attr('id').split('hour-',  [2]) //String method will be helpful.
-    timeBlockHour = ($(this).attr("id").split('hour-',[2]));
+    var timeBlockId = $thisTimeBlock.attr('id').split('hour-',  [2]);
+    timeBlockHour = ($(this).attr("id").split('hour-',[2])); //Initially an array is returned, and then the .split() method is used to remove the 'hour-' portion and target the number value to make a direct comparison with the current time in hours down below.
    
     
-  
+  // timeBlockId[1] targets the hour value assigned to each time block, and compares it directly to the current time in hours by selecting the appropriate 
   if (timeBlockId[1] == timeNow.hour()) {
       $(this).addClass('present');
       $(this).removeClass('past');
       $(this).removeClass('future');
-  }
+  } // CSS classes are either added or removed depending on the boolean output of true or false, relative to current time.
   else if (timeBlockId[1] > timeNow.hour()) {
       $(this).addClass('future');  
       $(this).removeClass('present');
@@ -91,7 +42,7 @@ $('#currentDay').text(timeNow.format('dddd, MMMM D YYYY, h:mm a'));
       $(this).removeClass('present');
   }
 
-//Targets the id and classes, and inputs the corresponding key values from local storage. 
+//Targets the id and classes, and inputs the corresponding key values from local storage. Currently unable to figure out how to loop through these values to simplify the code.
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -101,245 +52,7 @@ $("#hour-14 .description").val(localStorage.getItem("hour-14"));
 $("#hour-15 .description").val(localStorage.getItem("hour-15"));
 $("#hour-16 .description").val(localStorage.getItem("hour-16"));
 $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-  //  storageString = textAreaInput.toString()
-  //  console.log(storageString[5]);
-  //  textAreaInput.on('load', timeBlockId);
-  
+
   })
-  // var inputLoadNine = localStorage.getItem('hour-9');
-  // var inputLoadTen = localStorage.getItem('hour-10');
-  // var inputLoadEleven = localStorage.getItem('hour-11');
-  // var inputLoadTwelve = localStorage.getItem('hour-12');
-  // var inputLoadOne = localStorage.getItem('hour-13');
-  // var inputLoadTwo = localStorage.getItem('hour-14');
-  // var inputLoadThree = localStorage.getItem('hour-15');
-  // var inputLoadFour = localStorage.getItem('hour-16');
-  // var inputLoadFive = localStorage.getItem('hour-17');
-//  console.log(inputLoadFive)
 
-
-//  var getLocal = localStorage.getItem())
-
-
-//  console.log(textAreaInput);
-})
-
-// $('#hour9 .description').val(localStorage.getItem()));
-// localStorage.getItem(timeBlockId);
-
-
-
- 
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-
-
-
-// Added timeNow variable, which displays the current date/time within the header.
-
-
-// console.log(timeNow.hour());
-  
-// Added variables for the hours of the workday, assigning them to the IDs of their respetive hour blocks. Next step is hopefully to create conditional statements to determine color.
-// var nineAm = $("#hour-9")
-// var tenAm = $('#hour-10')
-// var elevenAm = $('#hour-11')
-// var twelvePm = $('#hour-12')
-// var onePm = $('#hour-1')
-// var twoPm = $('#hour-2')
-// var threePm = $('#hour-3')
-// var fourPm = $('#hour-4')
-// var fivePm = $('#hour-5')
-// var sixPm = $('#hour-6')
-
-// Created additional variables to assign each block a time using the .set() property of Day.js to be compared against the current global time in order to assess appropriate color status.
-// var nineAmTime = dayjs().set('hour', 9)
-// var tenAmTime  = dayjs().set('hour', 10)
-// var elevenAmTime = dayjs().set('hour', 11)
-// var twelvePmTime = dayjs().set('hour', 12).set('minute', 59).set('second', 59)
-// var onePmTime = dayjs().set('hour', 1).set('minute', 59).set('seconds', 59);
-// var twoPmTime = dayjs().set('hour', 2)
-// var threePmTime = dayjs().set('hour', 3)
-// var fourPmTime = dayjs().set('hour', 4)
-// var fivePmTime = dayjs().set('hour', 5)
-// var sixPmTime = dayjs().set('hour', 6)
-
-
-  // Added conditional 'if' statements for all blocks between 9am-6pm intended to check against local time, remove existing classes, and replace with appropriate classes where appropriate.
-  //Changed timeNow arguement to timeNow.hour() in order to compare solely the hour value of the timeNow object.
-
-  // if (timeNow.hour() == 9) {
-  //   nineAm.addClass('present');
-  //   nineAm.removeClass('past');
-  //   nineAm.removeClass('future');
-  // }
-  // if (timeNow.hour() < 9) {
-  //   nineAm.addClass('future');
-  //   nineAm.removeClass('past');
-  //   nineAm.removeClass('present');
-  // }
-  // if (timeNow.hour() > 9) {
-  //   nineAm.addClass('past');
-  //   nineAm.removeClass('present');
-  //   nineAm.removeClass('future');
-  // }
-  
-  
-  // if (timeNow.hour() == 10) {
-  //   tenAm.addClass('present');
-  //   tenAm.removeClass('past');
-  //   tenAm.removeClass('future');
-  // }
-  // if (timeNow.hour() < 10) {
-  //   tenAm.addClass('future');
-  //   tenAm.removeClass('past');
-  //   tenAm.removeClass('present');
-  // }
-  // if (timeNow.hour() > 10) {
-  //   tenAm.addClass('past');
-  //   tenAm.removeClass('present');
-  //   tenAm.removeClass('future');
-  // } 
-
-  // if (timeNow.hour() == 11) {
-  //   elevenAm.addClass('present');
-  //   elevenAm.removeClass('past');
-  //   elevenAm.removeClass('future');
-  // }
-  // if (timeNow.hour() < 11) {
-  //   elevenAm.addClass('future');
-  //   elevenAm.removeClass('past');
-  //   elevenAm.removeClass('present');
-  // }
-  // if (timeNow.hour() > 11) {
-  //   elevenAm.addClass('past');
-  //   elevenAm.removeClass('present');
-  //   elevenAm.removeClass('future');
-  // } 
-  
-  
-  // if (timeNow.hour() == 12) {
-  //   twelvePm.addClass('present');
-  //   twelvePm.removeClass('past');
-  //   twelvePm.removeClass('future');
-  // }
-  // if (timeNow.hour() < 12) {
-  //   twelvePm.addClass('future');
-  //   twelvePm.removeClass('past');
-  //   twelvePm.removeClass('present');
-    
-  // }
-  // if (timeNow.hour() > 12) {
-  //   twelvePm.addClass('past');
-  //   twelvePm.removeClass('present');
-  //   twelvePm.removeClass('future');
-    
-  // }
-
-  // if (timeNow.hour() == 13) {
-  //   onePm.addClass('present');
-  //   onePm.removeClass('past');
-  //   onePm.removeClass('future');
-  // }
-  // if (timeNow.hour() < 13) {
-  //   onePm.addClass('future');
-  //   onePm.removeClass('past');
-  //   onePm.removeClass('present');
-  // }
-  // if (timeNow.hour() > 13) {
-  //   onePm.addClass('past');
-  //   onePm.removeClass('present');
-  //   onePm.removeClass('future');
-  // }
-
-  // if (timeNow.hour() == 14) {
-  //   twoPm.addClass('present');
-  //   twoPm.removeClass('past');
-  //   twoPm.removeClass('future');
-  // }
-  // if (timeNow.hour() < 14) {
-  //   twoPm.addClass('future');
-  //   twoPm.removeClass('past');
-  //   twoPm.removeClass('present');
-  // }
-  // if (timeNow.hour() > 14) {
-  //   twoPm.addClass('past');
-  //   twoPm.removeClass('present');
-  //   twoPm.removeClass('future');
-  // }
-
-
-  // if (timeNow.hour() == 15) {
-  //   threePm.addClass('present');
-  //   threePm.removeClass('past');
-  //   threePm.removeClass('future');
-  // }
-  // if (timeNow.hour() < 15) {
-  //   threePm.addClass('future');
-  //   threePm.removeClass('past');
-  //   threePm.removeClass('present');
-  // }
-  // if (timeNow.hour() > 15) {
-  //   threePm.addClass('past');
-  //   threePm.removeClass('present');
-  //   threePm.removeClass('future');
-  // }
-
-
-  // if (timeNow.hour() == 16) {
-  //   fourPm.addClass('present');
-  //   fourPm.removeClass('past');
-  //   fourPm.removeClass('future');
-  // }
-  // if (timeNow.hour() < 16) {
-  //   twoPm.addClass('future');
-  //   fourPm.removeClass('past');
-  //   fourPm.removeClass('present');
-  // }
-  // if (timeNow.hour() > 16) {
-  //   fourPm.addClass('past');
-  //   fourPm.removeClass('present');
-  //   fourPm.removeClass('future');
-  // }
-
-
-  // if (timeNow.hour() == 17) {
-  //   fivePm.addClass('present');
-  //   fivePm.removeClass('past');
-  //   fivePm.removeClass('future');
-  // }
-  // if (timeNow.hour() < 17) {
-  //   fivePm.addClass('future');
-  //   fivePm.removeClass('past');
-  //   fivePm.removeClass('present');
-  // }
-  // if (timeNow.hour() > 17) {
-  //   fivePm.addClass('past');
-  //   fivePm.removeClass('present');
-  //   fivePm.removeClass('future');
-  // }
-
-
-  // if (timeNow.hour() == 18) {
-  //   sixPm.addClass('present');
-  //   sixPm.removeClass('past');
-  //   sixPm.removeClass('future');
-  // }
-  // if (timeNow.hour() < 18) {
-  //   sixPm.addClass('future');
-  //   sixPm.removeClass('past');
-  //   sixPm.removeClass('present');
-  // }
-  // if (timeNow.hour() > 18) {
-  //   sixPm.addClass('past');
-  //   sixPm.removeClass('present');
-  //   sixPm.removeClass('future');
-  // }
-
-
-// };
+});
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# scheduler
-A calendar application that allows you to save events for each hour of the day.
+# Work Day Personal Planner
+This Work Day Personal Planner is a calendar application that allows you to save events for each hour of the day, and displays colors for each hour that correspond with your local time.
+
+Using starter-code as the base of this application, I have added my own code in order to enable functionality by using the Day.js libray to enable accurate tracking of local time, adjusting color schemes to correspond with the current time, and allowing the user to save their calendar events to local storage for reference throughout their day.
+
+## Installation
+
+This application has been deployed via GitHub Pages, and can be used by following the deployment link within this GitHub repository, or directly through this URL: https://github.com/claxCode/scheduler . Additionally, this application may be accessed by downloading the files locally and opening them within VSCode, or equivalent software.
+
+## Functionality
+
+When interacting with the application, the will take note of the different colors of each hourly time block. The color representation is as follows:
+
+* Green = The hourly time block has yet to occur, and is in the future.
+* Red = The user is currently within this hourly time block.
+* Grey = The hour has passed and will remain grey until the next day begins.
+
+Within each respective hourly time block there is a text-area with an input field that allows you to schedule your time by adding events to your planner with text. Simply click the field, and type! Once you have completed your entry, click on the floppy disk icon to save what you have written. Entries will be saved to local storage, and will remain in the application if the window is refreshed. To remove events, delete them manually with your keyboard, or clear your browser's local storage.
+
+## Screenshots
+
+### Mock-Up
+
+![Mock-Up provided with codebase](/Assets/05-third-party-apis-homework-demo.gif)
+
+### Final Product
+
+![Finalized Code (taken before 9am)](/Assets/127.0.0.1_5500_index.html.png)
+
+## Acknowledgements
+
+Starter code provided by the Georgia Tech Full-Stack Coding Bootcamp and the edX team. Assist and support was provided by edX tutors and AskBCS Learning Assistants when tackling certain areas of friction.
+
+## Usage
+
+The rights to all code contained within belongs to their respective creators, though the product should be considered free for personal use.
